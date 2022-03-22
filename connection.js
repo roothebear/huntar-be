dac9d913a713b1b9d40667d4c88ff29e3c137052
Kalum Wildig <kalumwildig@Kalums-MacBook-Pro.local>
2022-03-22 15:37:59 +0000

    seed functionality added

--- connection.js
+++ connection.js
@@ -5,9 +5,6 @@ module.exports = () => {
     const uri = process.env.DB_URI;
     console.log(uri)
     connect(uri, {
-        // dbName: process.env.DB_NAME,
-        // user: process.env.DB_USER,
-        // pass: process.env.DB_PASSWORD,
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const { PORT = 9090 } = process.env;
+const { PORT = 3050 } = process.env;
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)
