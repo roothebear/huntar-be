3862d7e77ae0847998ed851415a8df72859ef010
faizakhancode <faizakhan488@hotmail.co.uk>
2022-03-24 12:35:48 +0000

    it will send a friendly error message

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,6 +1,7 @@
 const customError = (err, req, res, next) => {
+  console.log(err.message);
   if (err.name === 'CastError' || err.name === 'ValidationError') {
-    res.status(400).send({ msg: 'Bad request' });
+    res.status(400).send({ msg: err.message });
   } else next(err);
 };
 
