8222f8eacfabd9e4425caa30e5890067ca20050a
faizakhancode <faizakhan488@hotmail.co.uk>
2022-03-24 10:08:05 +0000

    it will ammend the run seed and production data

--- data/run-seed.js
+++ data/run-seed.js
@@ -1,15 +1,19 @@
 const seedDB = require('./seed');
 const testData = require('./test-data.json');
+const productionData = require('./production-data.json');
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost:27017/ScavengeARtest').then(() => {
-    return seedDB(testData)
-})
-.then(() => {
-    return mongoose.disconnect()
-})
-.then(() => {
-    console.log(`successfully disconnected`)
-})
-.catch((err) => console.log(err))
+mongoose
+  .connect(
+    'mongodb+srv://admin-user:banana123@cluster0.1cbrn.mongodb.net/ScavengeAR?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    return seedDB(productionData);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log(`successfully disconnected`);
+  })
+  .catch((err) => console.log(err));
